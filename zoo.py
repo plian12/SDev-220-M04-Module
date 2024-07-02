48a11763d3bcf904f5a15d027ea3f89b3d8ddfe5
plian12 <164810385+plian12@users.noreply.github.com>
2024-07-02 10:32:01 -0400

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,19 @@
+#11.1
+def hours():
+    print("Open 9-5 daily")
+
+import zoo 
+zoo.hours()
+
+#11.2
+import zoo as menagerie
+menagerie.hours()
+
+#11.3
+from zoo import hours
+hours()
+
+#11.4
+from zoo import hours as info
+info()
+
